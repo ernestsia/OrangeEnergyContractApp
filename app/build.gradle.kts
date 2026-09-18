78778540980db79b0bb058829c5e326acab78504
Ernest <your-github-kezeleeernest@gmail.com>
2026-09-18 13:39:53 +0000

    Specify Android and Kotlin plugin versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,31 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("com.android.application") version "8.2.2"
+    id("org.jetbrains.kotlin.android") version "1.9.22"
 }
 
 android {
-    namespace = "com.orange.energycontract"
+    namespace = "com.example.orangeenergycontractapp"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.orange.energycontract"
+        applicationId = "com.example.orangeenergycontractapp"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -28,4 +39,5 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
